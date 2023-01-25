f9791037fb952a97fe8ea50619cdae2a48e2ca8e
Osmancan Akagündüz <83430594+osmancan23@users.noreply.github.com>
2023-01-25 16:36:03 +0300

    added ui templates

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,20 +1,49 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+const replaceTemplate = require("./modules/replace_template");
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObj = JSON.stringify(data);
+const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
-  var pathname = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  console.log(query);
 
   if (pathname == "/" || pathname == "/overview") {
-    res.end("This is overview");
-  } else if (pathname == "/home") {
-    res.end("This is home");
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    let cardsHtml = dataObj
+      .map((e) => replaceTemplate(templateCard, e))
+      .join("");
+
+    let overview = templateOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+
+    res.end(overview);
+  } else if (pathname == "/product") {
+    const product = dataObj[query.id];
+
+    const productTemplate = replaceTemplate(templateProduct, product);
+
+    res.end(productTemplate);
   } else if (pathname == "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
-    res.end(JSON.parse(dataObj));
+    res.end(dataObj);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
